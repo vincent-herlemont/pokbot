768d6c34b0f8fac38b14e7ac3d8697a195d14be4
OpenShift guest <vincentherl@live.fr>
2013-11-07 08:59:15 -0500

    Jquery Cookie

--- js/logged.js
+++ js/logged.js
@@ -10,7 +10,16 @@ var XHR = function(method, ad, params) {
 		}
 	xhr.send( str );
 }
+
+function displayGrid(){
+   XHR('GET', '/patate', {onload: function() {
+       var grille = $.parseJson(this.responseText);
+       console.log(grille); 
+       ; } } );
+}
+
 console.log("test");
+
 function init() {
 	// Connect to the SocketIO server to retrieve ongoing games.
 	socket = io.connect();
